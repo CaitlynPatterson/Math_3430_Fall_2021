f2a305e24077bef538ca0e8cd90bcc3299a7ed63
CaitlynPatterson <90790287+CaitlynPatterson@users.noreply.github.com>
2021-09-15 12:35:19 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+a = [1, "two", 3.0, "1", 1.0]
+
+#only 80 characters per line#
+
+vector_01 = [1,0,0]
+vector_02 = [0,1,0]
+vector_03 = [0,0,1]
+
+# I will assume vectors are column vectors, unless otherwise stated
+
+matrix_3x3 = [vector_02, vector_03, vector_01]
+"""
+    matrix_3x3 = 
+    0 0 1
+    1 0 0
+    0 1 0
+"""
+
+print(matrix_3x3)
+len(matrix_3x3)
+print(len(matrix_3x3))
+print(len(vector_01))
+print(vector_01[1])
+
+
+
+
+
+
+
+
+    
